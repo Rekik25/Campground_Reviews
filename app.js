b0164d989e080d9ba6c11076d7b0244fc0f05911
Rohit Kumar <randomand03@gmail.com>
2020-06-19 19:53:53 +0530

    add seeds function, add partials

--- app.js
+++ app.js
@@ -10,21 +10,10 @@ var express     = require("express"),
 seedDB();
 mongoose.connect("mongodb://localhost/yelp_camp_v4", { useNewUrlParser: true,useUnifiedTopology: true });
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname+"/public"));
 app.set("view engine", "ejs");
 //SCHEMA SETUP
 
-
-// Campground.create(
-//         {name:"granite hills",
-//         image:"https://images.unsplash.com/photo-1487730116645-74489c95b41b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-//         description:"a huge granite hills, no bathrooms, no nothings, you gotta"
-//         },
-//           function(err, campground){
-//             if(err){  console.log(err);     }
-//             else {console.log("Newly Created Campground: ");
-//                 console.log(campground);
-//             }
-// });
 
 var campgrounds= [
     {name:"salmon reek",image:"https://images.unsplash.com/photo-1487730116645-74489c95b41b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"}, 
--- seeds.js
+++ seeds.js
@@ -5,15 +5,15 @@ var Comment     = require("./models/comment");
 var data = [
   {name: "Cloud's Rest", 
   image:"https://images.unsplash.com/photo-1487730116645-74489c95b41b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-  description: "bla bla bla"
+  description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis eligendi, optio soluta tempora tenetur eum eveniet ex nulla corrupti mollitia, maxime vero, perferendis praesentium quas quibusdam dolorem! Doloribus, accusantium saepe?"
   },
   {name: "Cloud's Rest 1.1", 
   image:"https://images.unsplash.com/photo-1530541930197-ff16ac917b0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-  description: "bla bla bla"
+  description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis eligendi, optio soluta tempora tenetur eum eveniet ex nulla corrupti mollitia, maxime vero, perferendis praesentium quas quibusdam dolorem! Doloribus, accusantium saepe?"
   },
   {name: "Cloud's Rest 1.2", 
   image:"https://images.unsplash.com/photo-1475483768296-6163e08872a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-  description: "bla bla bla"
+  description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis eligendi, optio soluta tempora tenetur eum eveniet ex nulla corrupti mollitia, maxime vero, perferendis praesentium quas quibusdam dolorem! Doloribus, accusantium saepe?"
   },
 ]
 function seedDB(){
